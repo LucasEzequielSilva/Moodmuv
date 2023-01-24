bd2e6a5146f662ccf7c2482c2f5af9db35021b9c
unknown <luxassilva@gmail.com>
2023-01-24 13:40:52 -0300

    1.0.5

--- controllers/sendEmail.ts
+++ controllers/sendEmail.ts
@@ -17,24 +17,24 @@ const sendEmail = async (email:string, uniqueString:string, verify:string) => {
 		to: email,
 		subject: "Hello, friend! Let's verify your email!",
 		html: `
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="flex justify-center items-center my-4">
-  <div class="w-96 h-56 bg-white rounded-3xl shadow-sm">
-    <div class="w-full h-[40%] bg-gradient-to-r from-[#573E81] to-[#6E5D8B] rounded-t-3xl flex justify-center items-center">
-      <img class="w-20" src="https://i.ibb.co/dWTTRPp/logo-MOOD-MUV-ok-1-1.png" alt="logo-MOOD-MUV-ok-1-1" border="0">
-    </div>
-    <div class="flex flex-col gap-1 px-4 py-2">
-      <h4 class="font-bold text-xl">Verifica tu correo electrónico</h4>
-      <p class="text-sm">Hola Muv, verifica tu correo para tener acceso a mas funcionalidades de Moodmuv</p>
-      <a class="rounded text-center bg-gradient-to-r from-[#573E81] to-[#6E5D8B] w-full p-2 text-white font-bold" href='http://moodmuv-back-production.up.railway.app/api/auth/${verify}/${uniqueString}'>Verificar</a>
-    </div>
-  </div>
-</body>
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="https://cdn.tailwindcss.com"></script>
+	  </head>
+	  <body class="flex justify-center items-center my-4">
+		<div style="border-radius: 1rem; width: 30rem; height: 15rem; box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
+		  <div style="width: 100%; height: 40%; background: linear-gradient(88.35deg, #573E81 0.84%, #6E5D8B 98.46%);
+		  border-radius: 1rem  1rem 0 0;" class="w-full h-[40%] bg-gradient-to-r from-[#573E81] to-[#6E5D8B] rounded-t-3xl flex justify-center items-center">
+			<img style="width:4.5rem;" src="https://i.ibb.co/dWTTRPp/logo-MOOD-MUV-ok-1-1.png" alt="logo-MOOD-MUV-ok-1-1" border="0">
+		  </div>
+		  <div style="display: flex; flex-direction: column; gap: .5rem; padding: .5rem 1rem;">
+			<h4 style="font-weight: bold; font-size: large;">Verifica tu correo electrónico</h4>
+			<p style="font-size:small">Hola Muv, verifica tu correo para tener acceso a mas funcionalidades de Moodmuv</p>
+			<a style="background: linear-gradient(88.35deg, #573E81 0.84%, #6E5D8B 98.46%); border-radius: .1rem; text-align: center; font-weight: bold; color: #fff; padding: .5rem;" href='http://moodmuv-back-production.up.railway.app/api/auth/${verify}/${uniqueString}'>Verificar</a>
+		  </div>
+		</div>
+	  </body>
 		`
 	}
 
